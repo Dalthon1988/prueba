6437be2a8625993904348366b049b549247bef3f
Dalthon1988 <dalthon1988@gmail.com>
2022-08-03 22:01:55 -0300

    Templates agregados; Archivos estaticos configurados

--- src/coder/views.py
+++ src/coder/views.py
@@ -1,9 +1,23 @@
+from ast import Return
 from django.shortcuts import render
 from django.http import HttpResponse
-from coder.models import Curso
+from coder.models import Curso, Estudiante,Profesor,Entregable
 
 
-def lista_cursos(request):
+def inicio(request):
+    return render(request,"coder/index.html", {"mensaje":"La comision 40150 es la mejor!"})
+
+def estudiante(request):
+    return HttpResponse('Vista del estudiante')
+
+
+def profesor(request):
+    return HttpResponse('Vista del profesor')
+
+def entregable(request):
+    return HttpResponse('Vista del entregable')
+
+def cursos(request):
 
     cursos = Curso.objects.all()
     
